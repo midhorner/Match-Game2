847450a8dfd01ab833d2671421b4ef308abf7910
Andrew Horner <andrew.e.horner@gmail.com>
2020-03-04 22:06:46 -0500

    added shuffler, added game reset, needs styling...add flames, add win game stuff

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var isFlipped = false;
 var boardLock = false;
 var firstCard;
 var secondCard;
+var matchNumber = 0;
 
 function flipCard() {
-  if (boardLock) {return};
-  if (this === firstCard) {return};
+  if (boardLock || this === firstCard) {
+    return;
+  }
 
   this.classList.add("flip");
 
@@ -29,8 +31,16 @@ function isMatch() {
 }
 
 function cardLock() {
+  matchNumber++;
+  console.log("number of matches = " + matchNumber);
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  if (matchNumber === 6) {
+    setTimeout(() => {
+      setNewGame();
+    }, 1000);
+  }
 
   resetGame();
 }
@@ -47,6 +57,22 @@ function unFlip() {
 function resetGame() {
   [isFlipped, boardLock] = [false, false];
   [firstCard, secondCard] = [null, null];
+  console.log("reset ran");
 }
+
+function setNewGame() {
+  matchNumber = 0;
+  CARDS.forEach(card => card.classList.remove("flip"));
+  CARDS.forEach(card => card.addEventListener("click", flipCard));
+  shuffle();
+}
+
+function shuffle() {
+  CARDS.forEach(card => {
+    let random = Math.floor(Math.random() * 12);
+    card.style.order = random;
+  });
+}
+shuffle();
 
 CARDS.forEach(card => card.addEventListener("click", flipCard));
